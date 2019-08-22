0dd511724982eb2e2feea6180fc97d30e95b311d
Christian Dullweber <dullweber@chromium.org>
2019-08-22 16:52:23 +0200

    fix ff

--- sharedWorker.js
+++ sharedWorker.js
@@ -3,15 +3,16 @@ let value = null;
 
 // This shared worker allows to store and retrieve values across tabs.
 self.onconnect = e => {
-    let port = e.ports[0];
-    clients.push(port);
-    console.log(e);
-    port.onmessage = e => {
-      if(e.data.value)
-        value = e.data.value;
-      else 
-        e.srcElement.postMessage({"value" : value});
-    };
-    port.start();
-    port.postMessage({"connected": true});
+  let port = e.ports[0];
+  clients.push(port);
+  port.onmessage = e => {
+    if (e.data.value) {
+      value = e.data.value;
+    } else {
+      for (let client of clients)
+        client.postMessage({ "value": value });
+    }
+  };
+  port.start();
+  port.postMessage({ "connected": true });
 };
