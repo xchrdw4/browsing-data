44dd4211dfbdc3e89bcd5482d996075c0ecb41af
Christian Dullweber <dullweber@chromium.org>
2021-02-22 15:41:21 +0100

    add navigator storage and worker test

---
+++ worker.js
@@ -0,0 +1,42 @@
+async function getNavigatorStorage() {
+  let d = await navigator.storage.getDirectory();
+  let h = await d.getFileHandle("worker.txt");
+  let f = await h.getFile();
+  return await f.text();
+}
+
+async function setNavigatorStorage(x)  {
+  let d = await navigator.storage.getDirectory();
+  let h = await d.getFileHandle("worker.txt", {create:true});
+  let w = await h.createWritable();
+  w.write(x);
+  w.close();
+}
+
+let datatypes = {
+  "Navigator.storage": [getNavigatorStorage, setNavigatorStorage],
+}
+
+// Call with get/set, type, [value]
+onmessage = async function(e) {
+    console.log('Worker: Message received from main script');
+    if (e.data[0] == 'get') {
+      let t = e.data[1];
+      let data;
+      try {
+        data = await datatypes[t][0]();
+      } catch (e) {
+        console.log("error reading data for " + t, e);
+      }
+      postMessage(data);
+    }
+    if (e.data[0] == 'set') {
+      let t = e.data[1];
+      try {
+        data = await datatypes[t][1](e.data[2]);
+      } catch (e) {
+        console.log("error writing data for " + t, e);
+      }
+      postMessage(data);
+    }
+  }
